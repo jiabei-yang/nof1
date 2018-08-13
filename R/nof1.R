192c8fd88f48185e046875d74377e8c221dda1ac
Michael Seo <swj8874@gmail.com>
2018-08-13 17:40:32 -0400

    revision

--- R/nof1.R
+++ R/nof1.R
@@ -1,3 +1,14 @@
+#' mcnet: A package for N of 1 study analysis using Bayesian methods
+#'
+#' A package for running N of 1 study trials
+#'
+#' An N of 1 trial is a clinical trial in which a single patient is the entire trial, a single case study.
+#' The main purpose of this package was to serve as an analysis tool for one of the PCORI grants we were working with.
+#' It is designed for N of 1 trials and can fit bayesian versions of linear regression, logistic/ordinal regression, and poisson regression.
+#'
+#' @docType package
+#' @name nof1-package
+#'
 #' @import coda
 #' @import MASS
 #' @import ggplot2
