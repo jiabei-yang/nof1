950c5114cfab3a350c214b0626355bbe2557fc6d
Michael Seo <swj8874@gmail.com>
2018-11-02 01:57:15 -0400

    revision

--- R/nof1.simulation.R
+++ R/nof1.simulation.R
@@ -80,6 +80,11 @@ nof1.poisson.simulation <- function(Base.size = 14, Treat.size = 56, alpha = 1, 
   list(Y = Y, Treat = Treat)
 }
 
+#' Normal data simulation
+#' 
+#' Simulating sample normal data
+#'
+#' @export
 
 nof1.normal.simulation <- function(Base.size = 2, Treat.size = 8, prec = 0.5, alpha = 50, beta_A = -3, beta_B = -1){
   
